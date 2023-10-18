07f578714166641db99aaa28accb594173386893
arakur <arakur65536@gmail.com>
2023-10-18 14:22:55 +0900

    Fixed conflict.

--- src/Parser/Expr.fs
+++ src/Parser/Expr.fs
@@ -4,7 +4,7 @@ open FSharpPlus
 
 let rec tryFrom (intermediate: IntermediateExpr) =
     match intermediate with
-    | IntermediateExpr.Numeral(value, measure) -> Ok(Script.Expr.Numeral(value, measure)) 
+    | IntermediateExpr.Numeral(value, measure) -> Ok(Script.Expr.Numeral(value, measure))
     | IntermediateExpr.String(value) -> Ok(Script.Expr.String value)
     | IntermediateExpr.Variable(value) -> Ok(Script.Expr.Variable value)
     | IntermediateExpr.App(func, args) ->
@@ -25,35 +25,30 @@ let rec tryFrom (intermediate: IntermediateExpr) =
 
             return Script.Expr.App(func', args')
         }
-    | IntermediateExpr.BinOpSeries(init: (IntermediateExpr * BinaryOperator) list, last) ->
+    | IntermediateExpr.BinOpSeries(init: (IntermediateExpr * Script.BinaryOperator) list, last) ->
         // TODO: Give this from outside.
         let binOpDictionary =
-            [ [ BinaryOperator.Other "&&"; BinaryOperator.Other "||" ], Associativity.Left
-              [ BinaryOperator.Other "="
-                BinaryOperator.Other "<"
-                BinaryOperator.Other ">"
-                BinaryOperator.Other "<="
-                BinaryOperator.Other ">=" ],
-              Associativity.None
-              [ BinaryOperator.Other "+"; BinaryOperator.Other "-" ], Associativity.Left
-              [ BinaryOperator.Other "*"; BinaryOperator.Other "/" ], Associativity.Left ]
+            [ [ Script.BinaryOperator.Other "&&"; Script.BinaryOperator.Other "||" ], Script.Associativity.Left
+              [ Script.BinaryOperator.Other "="
+                Script.BinaryOperator.Other "<"
+                Script.BinaryOperator.Other ">"
+                Script.BinaryOperator.Other "<="
+                Script.BinaryOperator.Other ">=" ],
+              Script.Associativity.None
+              [ Script.BinaryOperator.Other "+"; Script.BinaryOperator.Other "-" ], Script.Associativity.Left
+              [ Script.BinaryOperator.Other "*"; Script.BinaryOperator.Other "/" ], Script.Associativity.Left ]
 
         let rec fold
-            (dictionary: (BinaryOperator list * Associativity) list)
-            (init: (IntermediateExpr * BinaryOperator) list, last: IntermediateExpr)
-            : Result<Expr, string> =
+            (dictionary: (Script.BinaryOperator list * Script.Associativity) list)
+            (init: (IntermediateExpr * Script.BinaryOperator) list, last: IntermediateExpr)
+            : Result<Script.Expr, string> =
             match dictionary with
             | [] ->
                 match init with
                 | [] -> tryFrom last
-                | _ ->
-                    Error(
-                        snd init.Head
-                        |> function
-                            | BinaryOperator.Other s -> sprintf "Unknown binary operator `%s`." s
-                    )
+                | _ -> Error((snd init.Head).Name |> sprintf "Unknown binary operator `%s`.")
             | (operators, associativity) :: restDictionary ->
-                let rec tryCollect current (init: (IntermediateExpr * BinaryOperator) list) =
+                let rec tryCollect current (init: (IntermediateExpr * Script.BinaryOperator) list) =
                     match init with
                     | [] -> Ok([], (current, last))
                     | (e, op) :: rest when operators |> Seq.contains op ->
@@ -69,7 +64,7 @@ let rec tryFrom (intermediate: IntermediateExpr) =
                         fold restDictionary lastPart
                     else
                         match associativity with
-                        | Associativity.Left ->
+                        | Script.Associativity.Left ->
                             parts
                             |> Seq.map (fun (init', last', op') ->
                                 fold restDictionary (init', last') |> Result.map (fun expr -> expr, op'))
@@ -91,7 +86,7 @@ let rec tryFrom (intermediate: IntermediateExpr) =
                                         let! lastExpr = fold restDictionary lastPart
                                         return Script.Expr.BinaryOperator(op, expr, lastExpr)
                                     }))
-                        | Associativity.Right ->
+                        | Script.Associativity.Right ->
                             parts
                             |> Seq.map (fun (init', last', op') ->
                                 fold restDictionary (init', last') |> Result.map (fun expr -> expr, op'))
@@ -110,7 +105,7 @@ let rec tryFrom (intermediate: IntermediateExpr) =
                                         let! acc' = acc
                                         return Script.Expr.BinaryOperator(op, expr, acc')
                                     }))
-                        | Associativity.None ->
+                        | Script.Associativity.None ->
                             match parts with
                             | [ init', last', op' ] ->
                                 monad {
@@ -125,7 +120,7 @@ let rec tryFrom (intermediate: IntermediateExpr) =
                                         "An operator `%s` is not associative."
                                         (parts.Head
                                          |> function
-                                             | (_, _, BinaryOperator.Other s) -> s)
+                                             | (_, _, op) -> op.Name)
                                 ))
 
         fold binOpDictionary (init, last)
